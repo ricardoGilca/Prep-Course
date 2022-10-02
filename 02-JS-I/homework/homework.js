48b52286f27edb0229eda624d4b82a28eaeb8d00
unknown <tradergilca@gmail.com>
2022-10-01 23:01:05 -0500

    First Change

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = "RicardoGil";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "5";
+const nuevoNum = 5;
 
 // Crea una variable booleana:
-const nuevoBool = "false";
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - "5" === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * "4" === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === "1";
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
